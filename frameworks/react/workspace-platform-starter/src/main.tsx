84957d8861b63474e376dd56e4f0c03fb17f4dd6
Ray Cashmore <ray.cashmore@here.io>
2025-05-30 11:46:45 +1000

    Cleanup provider

--- frameworks/react/workspace-platform-starter/src/main.tsx
+++ frameworks/react/workspace-platform-starter/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { Provider } from './Provider';
+
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
