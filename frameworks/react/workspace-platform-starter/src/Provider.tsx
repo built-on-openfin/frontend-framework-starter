0c2827c4e84a5622c16921d9b72d473f4b21e0a6
Ray Cashmore <ray.cashmore@here.io>
2025-05-30 11:46:45 +1000

    Cleanup provider

--- frameworks/react/workspace-platform-starter/src/Provider.tsx
+++ frameworks/react/workspace-platform-starter/src/Provider.tsx
@@ -3,5 +3,13 @@ import { useOpenFin } from "./hooks/useOpenFin";
 export function Provider() {
 	useOpenFin();
 
-	return <h1>Provider</h1>;
+	return (
+		<div>
+			<h3>OpenFin platform provider</h3>
+			<p>
+				The window would usually be hidden, you can make it hidden on startup by setting the
+				platform.autoShow flag to false in the manifest.fin.json
+			</p>
+		</div>
+	);
 }
