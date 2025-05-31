13d84ea7cbf346d5cf5385a210f9e9a4d49ff8c6
Ray Cashmore <ray.cashmore@here.io>
2025-05-31 17:55:10 +1000

    Fix vite modules

--- frameworks/react/workspace-platform-starter/vite.config.ts
+++ frameworks/react/workspace-platform-starter/vite.config.ts
@@ -1,10 +1,58 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import path from 'path';
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import { fileURLToPath } from 'url';
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const entryDefinitions = [
+	{ entry: './openfin/modules/auth/example/index.ts', outputPathRelative: 'js/modules/auth', outputFilenameKey: 'example' },
+	{ entry: './openfin/modules/endpoint/local-storage/index.ts', outputPathRelative: 'js/modules/endpoint', outputFilenameKey: 'local-storage' },
+	{ entry: './openfin/modules/endpoint/channel/index.ts', outputPathRelative: 'js/modules/endpoint', outputFilenameKey: 'channel' },
+	{ entry: './openfin/modules/endpoint/inline-apps/index.ts', outputPathRelative: 'js/modules/endpoint', outputFilenameKey: 'inline-apps' },
+	{ entry: './openfin/modules/init-options/interop/index.ts', outputPathRelative: 'js/modules/init-options', outputFilenameKey: 'interop' },
+	{ entry: './openfin/modules/init-options/launch-app/index.ts', outputPathRelative: 'js/modules/init-options', outputFilenameKey: 'launch-app' },
+	{ entry: './openfin/modules/log/console/index.ts', outputPathRelative: 'js/modules/log', outputFilenameKey: 'console' },
+	{ entry: './openfin/modules/actions/opacity/index.ts', outputPathRelative: 'js/modules/actions', outputFilenameKey: 'opacity' },
+	{ entry: './openfin/modules/endpoint/example-connection-validation/index.ts', outputPathRelative: 'js/modules/endpoint', outputFilenameKey: 'example.connection.validation' },
+	{ entry: './openfin/modules/analytics/console/index.ts', outputPathRelative: 'js/modules/analytics', outputFilenameKey: 'console' },
+	{ entry: './openfin/modules/composite/developer/index.ts', outputPathRelative: 'js/modules/composite', outputFilenameKey: 'developer' },
+	{ entry: './openfin/modules/integrations/apps/index.ts', outputPathRelative: 'js/modules/integrations', outputFilenameKey: 'apps' },
+	{ entry: './openfin/modules/integrations/workspaces/index.ts', outputPathRelative: 'js/modules/integrations', outputFilenameKey: 'workspaces' },
+	{ entry: './openfin/modules/integrations/pages/index.ts', outputPathRelative: 'js/modules/integrations', outputFilenameKey: 'pages' },
+	{ entry: './openfin/modules/composite/about/index.ts', outputPathRelative: 'js/modules/composite', outputFilenameKey: 'about' },
+	{ entry: './openfin/modules/composite/pages/index.ts', outputPathRelative: 'js/modules/composite', outputFilenameKey: 'pages' },
+	{ entry: './openfin/modules/composite/windows/index.ts', outputPathRelative: 'js/modules/composite', outputFilenameKey: 'windows' },
+	{ entry: './openfin/modules/endpoint/example-context-processor/index.ts', outputPathRelative: 'js/modules/endpoint', outputFilenameKey: 'example.context.processor' },
+	{ entry: './openfin/modules/actions/custom-menu/index.ts', outputPathRelative: 'js/modules/actions', outputFilenameKey: 'custom-menu' },
+	{ entry: './openfin/modules/auth/openid-connect/index.ts', outputPathRelative: 'js/modules/auth', outputFilenameKey: 'openid-connect' },
+	{ entry: './openfin/modules/endpoint/favorite-local-storage/index.ts', outputPathRelative: 'js/modules/endpoint', outputFilenameKey: 'favorite-local-storage' },
+	{ entry: './openfin/modules/actions/favorites-menu/index.ts', outputPathRelative: 'js/modules/actions', outputFilenameKey: 'favorites-menu' },
+	{ entry: './openfin/modules/composite/include-in-snapshot/index.ts', outputPathRelative: 'js/modules/composite', outputFilenameKey: 'include-in-snapshot' },
+	{ entry: './openfin/modules/lifecycle/example-notification-service/index.ts', outputPathRelative: 'js/modules/lifecycle', outputFilenameKey: 'example-notification-service' },
+	{ entry: './openfin/modules/endpoint/example-notification-source/index.ts', outputPathRelative: 'js/modules/endpoint', outputFilenameKey: 'example-notification-source' },
+	{ entry: './openfin/modules/platform-override/application-url-and-access-validator/index.ts', outputPathRelative: 'js/modules/platform-override', outputFilenameKey: 'application-url-and-access-validator' },
+	{ entry: './openfin/modules/init-options/launch-workspace/index.ts', outputPathRelative: 'js/modules/init-options', outputFilenameKey: 'launch-workspace' },
+	{ entry: './openfin/modules/composite/default-workspace/index.ts', outputPathRelative: 'js/modules/composite', outputFilenameKey: 'default-workspace' },
+	{ entry: './openfin/modules/content-creation/view-position/index.ts', outputPathRelative: 'js/modules/content-creation', outputFilenameKey: 'view-position' },
+	{ entry: './openfin/modules/actions/window-platform/index.ts', outputPathRelative: 'js/modules/actions', outputFilenameKey: 'window-platform' },
+	{ entry: './openfin/modules/share/pages/index.ts', outputPathRelative: 'js/modules/share', outputFilenameKey: 'pages' },
+	{ entry: './openfin/modules/share/workspaces/index.ts', outputPathRelative: 'js/modules/share', outputFilenameKey: 'workspaces' },
+	{ entry: './openfin/modules/interop-override/wps-interop-override/index.ts', outputPathRelative: 'js/modules/interop-override', outputFilenameKey: 'wps-interop-override' },
+	{ entry: './openfin/modules/platform-override/wps-platform-override/index.ts', outputPathRelative: 'js/modules/platform-override', outputFilenameKey: 'wps-platform-override' },
+];
+
+const rollupInput = entryDefinitions.reduce((acc, item) => {
+	const name = path.join(item.outputPathRelative, item.outputFilenameKey).replace(/\\/g, '/');
+	acc[name] = item.entry;
+	return acc;
+}, {} as Record<string, string>);
+
+rollupInput['root_html'] = path.resolve(__dirname, 'index.html');
 
 export default defineConfig({
-	root: __dirname,
 	plugins: [
 		react(),
 		viteStaticCopy({
@@ -19,126 +67,23 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'workspace-platform-starter': path.resolve(__dirname, './openfin/framework')
-		}
+		},
+		extensions: ['.tsx', '.ts', '.js', '.json'],
 	},
 	server: {
-        port: 8080
-    },
+		port: 8080
+	},
 	build: {
-		outDir: "./dist",
-		emptyOutDir: true,
-		reportCompressedSize: true,
-		commonjsOptions: {
-			transformMixedEsModules: true
-		},
-		// lib: {
-		// 	entry: "src/index.ts",
-		// 	name: "openfin-modules",
-		// 	fileName: "index",
-		// 	formats: ["es"]
-		// },
+		outDir: path.resolve(__dirname, 'dist'),
+		sourcemap: false,
 		rollupOptions: {
-			external: ["fin", "react", "react-dom", "react/jsx-runtime"],
-			input: {
-				// Provider React app
-				main: path.resolve(__dirname, "src/main.tsx"),
-
-				// Modules
-				interop: path.resolve(__dirname, "./openfin/modules/init-options/interop/index.ts"),
-				example: path.resolve(__dirname, "./openfin/modules/auth/example/index.ts"),
-				"local-storage": path.resolve(__dirname, "./openfin/modules/endpoint/local-storage/index.ts"),
-				channel: path.resolve(__dirname, "./openfin/modules/endpoint/channel/index.ts"),
-				"inline-apps": path.resolve(__dirname, "./openfin/modules/endpoint/inline-apps/index.ts"),
-				"launch-app": path.resolve(__dirname, "./openfin/modules/init-options/launch-app/index.ts"),
-				console: path.resolve(__dirname, "./openfin/modules/log/console/index.ts"),
-				opacity: path.resolve(__dirname, "./openfin/modules/actions/opacity/index.ts"),
-				"example-connection-validation": path.resolve(
-					__dirname,
-					"./openfin/modules/endpoint/example-connection-validation/index.ts"
-				),
-				"analytics-console": path.resolve(__dirname, "./openfin/modules/analytics/console/index.ts"),
-				developer: path.resolve(__dirname, "./openfin/modules/composite/developer/index.ts"),
-				apps: path.resolve(__dirname, "./openfin/modules/integrations/apps/index.ts"),
-				workspaces: path.resolve(__dirname, "./openfin/modules/integrations/workspaces/index.ts"),
-				pages: path.resolve(__dirname, "./openfin/modules/integrations/pages/index.ts"),
-				about: path.resolve(__dirname, "./openfin/modules/composite/about/index.ts"),
-				"composite-pages": path.resolve(__dirname, "./openfin/modules/composite/pages/index.ts"),
-				windows: path.resolve(__dirname, "./openfin/modules/composite/windows/index.ts"),
-				"example-context-processor": path.resolve(
-					__dirname,
-					"./openfin/modules/endpoint/example-context-processor/index.ts"
-				),
-				"fdc3-mapper": path.resolve(__dirname, "./openfin/framework/fdc3/index.ts"),
-				"custom-menu": path.resolve(__dirname, "./openfin/modules/actions/custom-menu/index.ts"),
-				"openid-connect": path.resolve(__dirname, "./openfin/modules/auth/openid-connect/index.ts"),
-				"favorite-local-storage": path.resolve(
-					__dirname,
-					"./openfin/modules/endpoint/favorite-local-storage/index.ts"
-				),
-				"favorites-menu": path.resolve(__dirname, "./openfin/modules/actions/favorites-menu/index.ts"),
-				"include-in-snapshot": path.resolve(
-					__dirname,
-					"./openfin/modules/composite/include-in-snapshot/index.ts"
-				),
-				"example-notification-service": path.resolve(
-					__dirname,
-					"./openfin/modules/lifecycle/example-notification-service/index.ts"
-				),
-				"example-notification-source": path.resolve(
-					__dirname,
-					"./openfin/modules/endpoint/example-notification-source/index.ts"
-				),
-				"application-url-and-access-validator": path.resolve(
-					__dirname,
-					"./openfin/modules/platform-override/application-url-and-access-validator/index.ts"
-				),
-				"launch-workspace": path.resolve(
-					__dirname,
-					"./openfin/modules/init-options/launch-workspace/index.ts"
-				),
-				"default-workspace": path.resolve(
-					__dirname,
-					"./openfin/modules/composite/default-workspace/index.ts"
-				),
-				"view-position": path.resolve(__dirname, "./openfin/modules/content-creation/view-position/index.ts"),
-				"window-platform": path.resolve(__dirname, "./openfin/modules/actions/window-platform/index.ts"),
-				"share-pages": path.resolve(__dirname, "./openfin/modules/share/pages/index.ts"),
-				"share-workspaces": path.resolve(__dirname, "./openfin/modules/share/workspaces/index.ts"),
-				"wps-interop-override": path.resolve(
-					__dirname,
-					"./openfin/modules/interop-override/wps-interop-override/index.ts"
-				),
-				"openfin-cloud-interop": path.resolve(
-					__dirname,
-					"./openfin/modules/interop-override/openfin-cloud-interop/index.ts"
-				),
-				"wps-platform-override": path.resolve(
-					__dirname,
-					"./openfin/modules/platform-override/wps-platform-override/index.ts"
-				),
-				"snap-window-selection-override": path.resolve(
-					__dirname,
-					"./openfin/modules/platform-override/snap-window-selection-override/index.ts"
-				),
-				"get-user-decision-for-beforeunload": path.resolve(
-					__dirname,
-					"./openfin/modules/platform-override/get-user-decision-for-beforeunload/index.ts"
-				)
-			},
+			treeshake: false,
+			input: rollupInput,
+			external: ['fin'],
 			output: {
-				format: "es",
-				dir: path.resolve(__dirname, "dist"),
-				sourcemap: false,
-				manualChunks: undefined,
-				// preserveModules: true,
-				preserveModulesRoot: "openfin/modules",
-				entryFileNames: (chunkInfo) => {
-					if (chunkInfo.name === "main") {
-						return "[name].bundle.js"
-					}
-					return `js/modules/[name].bundle.js`;
-				}
-			}
-		}
-	}
+				format: 'es',
+				entryFileNames: '[name].bundle.js',
+			},
+		},
+	},
 });
