3cb963415a7f669c8faef7a903b35194e6080fed
Ray Cashmore <ray.cashmore@here.io>
2025-11-18 23:33:00 +1100

    Stock peers demo view

--- frameworks/react/workspace-platform-starter/src/app.tsx
+++ frameworks/react/workspace-platform-starter/src/app.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "./Provider";
+import { Monitor } from "./views/monitor";
 import { View1 } from "./views/view1";
 import { View2 } from "./views/view2";
 
@@ -10,6 +11,7 @@ export default function App() {
 				<Route path="/" element={<Provider />} />
 				<Route path="/view1" element={<View1 />} />
 				<Route path="/view2" element={<View2 />} />
+				<Route path="/stock-peers-monitor" element={<Monitor />}></Route>
 			</Routes>
 		</BrowserRouter>
 	);
--- frameworks/react/workspace-platform-starter/src/main.tsx
+++ frameworks/react/workspace-platform-starter/src/main.tsx
@@ -4,8 +4,4 @@ import App from "./app";
 
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
